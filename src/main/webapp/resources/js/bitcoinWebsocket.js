b76591f6cb474da15c0d950e0907d802ba47f5a4
jhblue97 <jhblue97@naver.com>
2021-09-19 13:13:34 +0900

    20210919 PC - 차트 가격 완성

--- src/main/webapp/resources/js/bitcoinWebsocket.js
+++ src/main/webapp/resources/js/bitcoinWebsocket.js
@@ -2,11 +2,15 @@
 /**
  * bitcoin websocket
  */
-function bitcoin_ko(){
 	
+/*onconnect = function (e) {
+	console.log('gggg');
 	let bitcoin_ko;
-	
-		var ws = new WebSocket("wss://api.upbit.com/websocket/v1");
+	var count = 0;
+ 	let port = e.ports[0];
+	port.onmessage = function (e) {
+
+		let ws = new WebSocket("wss://api.upbit.com/websocket/v1");
 					ws.binaryType = 'arraybuffer';				
 					ws.onopen = function(message) {
 						// 콘솔 텍스트에 메시지를 출력한다.
@@ -33,20 +37,27 @@ function bitcoin_ko(){
 						bitcoin_ko  = json_arr.trade_price;
 						console.log(bitcoin_ko);
 						//$('#ko_price').val(bitcoin_ko);
-						document.getElementById("ko_price").value = bitcoin_ko;
+						port.postMessage(bitcoin_ko);
 						//alert(bitcoin_ko);
 						//ws.close();
+				
 					}
+	}
+}*/
 
+var count = 0;
+onconnect = function (e) {
+// SharedWorker 쓰레드로 받는다. 어떤 스택, 어떤 thread를 실행하더라도 한 개의 쓰래드로 공유된다.
+let port = e.ports[0];
 
-/**
- * bitcoin websocket
- */
-function bitcoin_ko(){
+	let bitcoin_ko = 'dddd';
+	let count = 'ff';
 	
-	let bitcoin_ko;
 	
-		var ws = new WebSocket("wss://api.upbit.com/websocket/v1");
+// postMessage의 값을 받는다.
+port.onmessage = function (event) {
+	 console.log( 'Worker가 받은 메시지 ', event.data ); 
+	let ws = new WebSocket("wss://api.upbit.com/websocket/v1");
 					ws.binaryType = 'arraybuffer';				
 					ws.onopen = function(message) {
 						// 콘솔 텍스트에 메시지를 출력한다.
@@ -60,22 +71,19 @@ function bitcoin_ko(){
 						socket = undefined; 
 					}
 					ws.onmessage= function(e){ 
-						//console.log(e);
 						var enc = new TextDecoder("utf-8");
 						var arr = new Uint8Array(e.data);	
-						//console.log(arr);
 						var decoe_arr = enc.decode(arr);
-						//console.log(decoe_arr);
 						var json_arr =JSON.parse(decoe_arr);
-						//console.log(json_arr.trade_price);
-						
-						//bitcoin_ko  = JSON.stringify(json_arr.trade_price);
 						bitcoin_ko  = json_arr.trade_price;
-						console.log(bitcoin_ko);
-						//$('#ko_price').val(bitcoin_ko);
-						document.getElementById("ko_price").value = bitcoin_ko;
-						//alert(bitcoin_ko);
-						//ws.close();
+						port.postMessage( event.data);
 					}
+	
+// count를 증가 시켜 postMessage를 보낸다.
+
+
+
 
 }
+}
+
